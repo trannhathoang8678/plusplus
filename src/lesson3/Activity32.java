3a6be035033f156224409a5bc1aadd410b6238ac
trannhathoang8678 <trannhathoang8678@gmail.com>
2020-06-16 23:51:36 +0700

    Activity32

---
+++ src/lesson3/Activity32.java
@@ -0,0 +1,26 @@
+package lesson3;
+
+import java.util.Scanner;
+
+public class Activity32 {
+    public static void main(String[] args)
+    {
+        Scanner sc= new Scanner(System.in);
+        System.out.println("Input fibonacci lenght: ");
+        int n = sc.nextInt();
+        long firstfibo= 0,secondfibo=1,transfer;
+        System.out.print(firstfibo + ", " + secondfibo);
+        for(int i=3;i<=n;i++)
+        {
+            transfer = firstfibo;
+            firstfibo += secondfibo;
+            secondfibo = transfer;
+                System.out.print(", " + (secondfibo+firstfibo));
+
+        }
+
+
+
+
+    }
+}
