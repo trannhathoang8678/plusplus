3b0ab13fb3b7906d69d1f2bf2e0d98b01c7030f6
trannhathoang8678 <trannhathoang8678@gmail.com>
2020-06-17 01:03:01 +0700

    fixedActivity32

--- src/lesson3/Activity32.java
+++ src/lesson3/Activity32.java
@@ -16,7 +16,6 @@ public class Activity32 {
             firstfibo += secondfibo;
             secondfibo = transfer;
                 System.out.print(", " + (secondfibo+firstfibo));
-
         }
     }
 }
