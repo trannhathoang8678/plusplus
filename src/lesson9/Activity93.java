146e9c6d2fc166ed02f772559927ac17979333fb
trannhathoang8678 <trannhathoang8678@gmail.com>
2020-06-28 01:20:41 +0700

    Activity93

---
+++ src/lesson9/Activity93.java
@@ -0,0 +1,10 @@
+package lesson9;
+
+import java.io.File;
+
+public class Activity93 {
+    public static void main(String[] args) {
+        File file = new File("/home/trannhathoang8678/Documents/plusplus/file");
+        System.out.println(file.exists());
+    }
+}
