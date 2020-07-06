f4f822682cf7c246546ad504a38cdf0b04619a69
trannhathoang8678 <trannhathoang8678@gmail.com>
2020-07-06 10:42:20 +0700

    add input method in main

--- src/lesson5/activity53/test.java
+++ src/lesson5/activity53/test.java
@@ -4,15 +4,32 @@ import java.util.Scanner;
 
 public class test {
     public static void main(String[] args) {
-        Dictionary dictionary = new Dictionary();
+        System.out.println("Number of words: ");
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        Word[] words = new Word[n];
+        String key, value;
+        int type;
+        System.out.println("Write 0 for AV and 1 for VA");
+        for (int i = 0; i < n; i++) {
+            System.out.println("Word " + (i + 1));
+            System.out.print("Type: ");
+            type = sc.nextInt();
+            System.out.print("Key: ");
+            key = sc.nextLine();
+            key = sc.nextLine();
+            System.out.print("Value: ");
+            value = sc.nextLine();
+            words[i] = new Word(key, value, type);
+        }
+        Dictionary dictionary = new Dictionary(words);
         System.out.println("Number of AV: " + dictionary.numberofAV());
         System.out.println("Number of AV: " + dictionary.numberofVA());
         System.out.println("The word you want to find its meaning: ");
-        Scanner sc = new Scanner(System.in);
         String s = sc.nextLine();
         System.out.print("Type: ");
-        int type = sc.nextInt();
+         type = sc.nextInt();
         sc.close();
-        System.out.print("Meaning: " + dictionary.find(s,type));
+        System.out.print("Meaning: " + dictionary.find(s, type));
     }
 }
